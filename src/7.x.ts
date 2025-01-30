90ac826c98b6d5316bb7bd36c7331e15943d6021
mrkvon <mrkvon@protonmail.com>
2025-01-30 01:44:13 +0100

    refactor!: v7.getAuthenticatedFetch to use auth header instead of cookie
    This is a step towards removing tough-cookie, have browser compatibility.
    
    BREAKING CHANGE: also remove default values for v7.createAccount

--- src/7.x.ts
+++ src/7.x.ts
@@ -1,16 +1,238 @@
+/**
+ * These methods are based on CSS documentation
+ * https://communitysolidserver.github.io/CommunitySolidServer/latest/usage/client-credentials/
+ *
+ *
+Original license:
+
+MIT License
+
+Copyright © 2019–2024 Inrupt Inc. and imec
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 import {
   buildAuthenticatedFetch,
   createDpopHeader,
   generateDpopKeyPair,
+  KeyPair,
 } from '@inrupt/solid-client-authn-core'
 import tough from 'tough-cookie'
 import { toBase64 } from './utils.js'
 
-type AccountHandles = {
+interface AccountHandles {
   controls: {
     password: { login: string; create: string }
     account: { webId: string; pod: string; clientCredentials: string }
   }
+}
+
+const getAccountAuthorization = async ({
+  provider,
+  email,
+  password,
+  fetch: customFetch = globalThis.fetch,
+}: {
+  provider: string
+  email: string
+  password: string
+  fetch?: typeof globalThis.fetch
+}) => {
+  // First we request the account API controls to find out where we can log in
+  const indexResponse = await customFetch(new URL('.account/', provider))
+
+  await throwIfResponseNotOk(indexResponse)
+
+  const { controls } = (await indexResponse.json()) as AccountHandles
+
+  // And then we log in to the account API
+  const response = await customFetch(controls.password.login, {
+    method: 'POST',
+    headers: { 'content-type': 'application/json' },
+    body: JSON.stringify({ email, password }),
+  })
+
+  await throwIfResponseNotOk(response)
+
+  // This authorization value will be used to authenticate in the next step
+  const { authorization } = (await response.json()) as { authorization: string }
+  return authorization
+}
+
+const getIdAndSecret = async ({
+  authorization,
+  provider,
+  webId,
+  fetch: customFetch = globalThis.fetch,
+}: {
+  authorization: string
+  provider: string
+  webId?: string
+  fetch?: typeof globalThis.fetch
+}) => {
+  // Now that we are logged in, we need to request the updated controls from the server.
+  // These will now have more values than in the previous example.
+  const indexResponse = await customFetch(new URL('.account/', provider), {
+    headers: { authorization: `CSS-Account-Token ${authorization}` },
+  })
+
+  await throwIfResponseNotOk(indexResponse)
+
+  const handles = (await indexResponse.json()) as AccountHandles
+
+  // Here we request the server to generate a token on our account
+  const response = await customFetch(
+    handles.controls.account.clientCredentials,
+    {
+      method: 'POST',
+      headers: {
+        authorization: `CSS-Account-Token ${authorization}`,
+        'content-type': 'application/json',
+      },
+      // The name field will be used when generating the ID of your token.
+      // The WebID field determines which WebID you will identify as when using the token.
+      // Only WebIDs linked to your account can be used.
+      body: JSON.stringify({
+        name: 'my-token',
+        webId: await getWebId({
+          webId,
+          handles,
+          authorization,
+          fetch: customFetch,
+        }),
+      }),
+    },
+  )
+
+  await throwIfResponseNotOk(response)
+
+  // These are the identifier and secret of your token.
+  // Store the secret somewhere safe as there is no way to request it again from the server!
+  // The `resource` value can be used to delete the token at a later point in time.
+  const { id, secret /*, resource*/ } = (await response.json()) as {
+    id: string
+    secret: string
+    resource: string
+  }
+
+  return { id, secret }
+}
+
+const getWebId = async ({
+  webId,
+  handles,
+  authorization,
+  fetch: customFetch = globalThis.fetch,
+}: {
+  webId?: string
+  handles: AccountHandles
+  authorization: string
+  fetch?: typeof globalThis.fetch
+}) => {
+  // now let's get all available webIds
+
+  const webIdsResponse = await customFetch(handles.controls.account.webId, {
+    headers: {
+      authorization: `CSS-Account-Token ${authorization}`,
+    },
+  })
+
+  await throwIfResponseNotOk(webIdsResponse)
+
+  const webIdsBody = (await webIdsResponse.json()) as {
+    webIdLinks: { [key: string]: string }
+  }
+
+  const webIds = Object.keys(webIdsBody.webIdLinks)
+
+  if (webId) {
+    if (webIds.includes(webId)) return webId
+    else throw new Error("linked webIds don't include provided webId")
+  }
+
+  if (webIds.length === 0)
+    throw new Error('no webId associated with this account')
+
+  return webIds[0]
+}
+
+const getAccessToken = async ({
+  id,
+  secret,
+  provider,
+  fetch: customFetch = globalThis.fetch,
+}: {
+  id: string
+  secret: string
+  provider: string
+  fetch?: typeof globalThis.fetch
+}) => {
+  // A key pair is needed for encryption.
+  // This function from `solid-client-authn` generates such a pair for you.
+  const dpopKey = await generateDpopKeyPair()
+
+  // These are the ID and secret generated in the previous step.
+  // Both the ID and the secret need to be form-encoded.
+  const authString = `${encodeURIComponent(id)}:${encodeURIComponent(secret)}`
+  // This URL can be found by looking at the "token_endpoint" field at
+  // http://localhost:3000/.well-known/openid-configuration
+  // if your server is hosted at http://localhost:3000/.
+  const tokenUrl = new URL('.oidc/token', provider)
+  const response = await customFetch(tokenUrl, {
+    method: 'POST',
+    headers: {
+      // The header needs to be in base64 encoding.
+      authorization: `Basic ${toBase64(authString)}`,
+      'content-type': 'application/x-www-form-urlencoded',
+      dpop: await createDpopHeader(tokenUrl.toString(), 'POST', dpopKey),
+    },
+    body: 'grant_type=client_credentials&scope=webid',
+  })
+
+  await throwIfResponseNotOk(response)
+
+  // This is the Access token that will be used to do an authenticated request to the server.
+  // The JSON also contains an "expires_in" field in seconds,
+  // which you can use to know when you need request a new Access token.
+  const { access_token: accessToken } = (await response.json()) as {
+    access_token: string
+  }
+
+  return { accessToken, dpopKey }
+}
+
+const generateAuthenticatedFetch = async ({
+  accessToken,
+  dpopKey,
+  fetch: customFetch = globalThis.fetch,
+}: {
+  accessToken: string
+  dpopKey: KeyPair
+  fetch?: typeof globalThis.fetch
+}) => {
+  // The DPoP key needs to be the same key as the one used in the previous step.
+  // The Access token is the one generated in the previous step.
+  const authFetch = await buildAuthenticatedFetch(customFetch, accessToken, {
+    dpopKey,
+  })
+
+  return authFetch
 }
 
 export const getAuthenticatedFetch = async ({
@@ -26,136 +248,30 @@ export const getAuthenticatedFetch = async ({
   webId?: string
   fetch?: typeof globalThis.fetch
 }) => {
-  // log in first
-
-  const handlesResponse = await customFetch(
-    new URL('.account/', provider).toString(),
-  )
-  await throwIfResponseNotOk(handlesResponse)
-
-  const handles = (await handlesResponse.json()) as AccountHandles
-
-  const loginResponse = await customFetch(handles.controls.password.login, {
-    method: 'post',
-    body: JSON.stringify({ email, password }),
-    headers: { 'content-type': 'application/json' },
+  const authorization = await getAccountAuthorization({
+    provider,
+    email,
+    password,
+    fetch: customFetch,
   })
 
-  await throwIfResponseNotOk(loginResponse)
-
-  const jar = new tough.CookieJar()
-  const accountCookie = loginResponse.headers.get('set-cookie')
-
-  if (!accountCookie)
-    throw new Error('unexpectedly authorization cookie not available')
-  await jar.setCookie(accountCookie, provider)
-
-  // get cookie
-  const cookie = await jar.getCookieString(provider)
-
-  // This assumes your server is started under http://localhost:3000/.
-  // It also assumes you have already logged in and `cookie` contains a valid cookie header
-  // as described in the API documentation.
-  const indexResponse = await customFetch(
-    new URL('.account/', provider).toString(),
-    { headers: { cookie } },
-  )
-
-  await throwIfResponseNotOk(indexResponse)
-
-  const { controls } = (await indexResponse.json()) as AccountHandles
-
-  // now let's get all available webIds
-
-  const webIdsResponse = await customFetch(controls.account.webId, {
-    headers: { cookie },
+  const { id, secret } = await getIdAndSecret({
+    authorization,
+    provider,
+    webId,
+    fetch: customFetch,
   })
-
-  await throwIfResponseNotOk(webIdsResponse)
-
-  const webIdsBody = (await webIdsResponse.json()) as { webIdLinks: string }
-
-  const webIds = Object.keys(webIdsBody.webIdLinks)
-
-  // get web id that is linked to this account. We take one of the available, or the one provided in params
-  const getWebId = (webIds: string[], webId?: string) => {
-    if (webIds.length === 0)
-      throw new Error('no webId associated with this account')
-
-    if (!webId) return webIds[0]
-
-    if (!webIds.includes(webId))
-      throw new Error("linked webIds don't include provided webId")
-
-    return webId
-  }
-
-  const linkedWebId = getWebId(webIds, webId)
-
-  const response = await customFetch(controls.account.clientCredentials, {
-    method: 'POST',
-    headers: { cookie, 'content-type': 'application/json' },
-    // The name field will be used when generating the ID of your token.
-    // The WebID field determines which WebID you will identify as when using the token.
-    // Only WebIDs linked to your account can be used.
-    body: JSON.stringify({
-      name: 'my-token',
-      webId: linkedWebId,
-    }),
+  const { accessToken, dpopKey } = await getAccessToken({
+    id,
+    secret,
+    provider,
+    fetch: customFetch,
   })
-
-  await throwIfResponseNotOk(response)
-
-  // These are the identifier and secret of your token.
-  // Store the secret somewhere safe as there is no way to request it again from the server!
-  // The `resource` value can be used to delete the token at a later point in time.
-  const { id, secret /*, resource*/ } = (await response.json()) as {
-    id: string
-    secret: string
-  }
-
-  // **************************************************
-
-  // A key pair is needed for encryption.
-  // This function from `solid-client-authn` generates such a pair for you.
-  const dpopKey = await generateDpopKeyPair()
-
-  // These are the ID and secret generated in the previous step.
-  // Both the ID and the secret need to be form-encoded.
-  const authString = `${encodeURIComponent(id)}:${encodeURIComponent(secret)}`
-  // This URL can be found by looking at the "token_endpoint" field at
-  // http://localhost:3000/.well-known/openid-configuration
-  // if your server is hosted at http://localhost:3000/.
-  const tokenUrl = new URL('.oidc/token', provider).toString()
-  const response2 = await customFetch(tokenUrl, {
-    method: 'POST',
-    headers: {
-      // The header needs to be in base64 encoding.
-      authorization: `Basic ${toBase64(authString)}`,
-      'content-type': 'application/x-www-form-urlencoded',
-      dpop: await createDpopHeader(tokenUrl, 'POST', dpopKey),
-    },
-    body: 'grant_type=client_credentials&scope=webid',
-  })
-  await throwIfResponseNotOk(response2)
-
-  // This is the Access token that will be used to do an authenticated request to the server.
-  // The JSON also contains an "expires_in" field in seconds,
-  // which you can use to know when you need request a new Access token.
-  const { access_token: accessToken } = (await response2.json()) as {
-    access_token: string
-  }
-
-  // *********************************************
-
-  // The DPoP key needs to be the same key as the one used in the previous step.
-  // The Access token is the one generated in the previous step.
-  const authFetch = await buildAuthenticatedFetch(fetch, accessToken, {
+  return await generateAuthenticatedFetch({
+    accessToken,
     dpopKey,
+    fetch: customFetch,
   })
-  // authFetch can now be used as a standard fetch function that will authenticate as your WebID.
-  // This request will do a simple GET for example.
-  return authFetch
 }
 
 const throwIfResponseNotOk = async (response: Response) => {
@@ -165,6 +281,8 @@ const throwIfResponseNotOk = async (response: Response) => {
     )
 }
 
+// TODO get rid of the tough-cookie
+// use CSS-Account-Token header instead
 export const createAccount = async ({
   username,
   password,
@@ -173,13 +291,11 @@ export const createAccount = async ({
   fetch: customFetch = globalThis.fetch,
 }: {
   username: string
-  password?: string
-  email?: string
+  password: string
+  email: string
   provider: string
   fetch?: typeof globalThis.fetch
 }) => {
-  password ??= 'correcthorsebatterystaple'
-  email ??= username + '@example.org'
   const config = {
     idp: new URL('./', provider).toString(),
     podUrl: new URL(`${username}/`, provider).toString(),
